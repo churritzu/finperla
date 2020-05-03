b3177df0d0bfd69ab6744c39bbdf5fa3a6a703d8
churritzu <churritzu@yahoo.com>
2020-05-03 14:54:03 -0700

    Create a if/else statement for the flags
    Create a if/else statement for the flags and if not a flag pass then
    return the help menu options

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from modules.incomes.Income import Income
 from modules.cli.CLIargs import CLIArgs
 
 if __name__ == "__main__":
-	args = CLIArgs().getArgs()
+	cliargs = CLIArgs()
+	args = cliargs.getArgs()
+	
 	print(args)
+
+	if(args.income): print("The Income flag has called", args.income)
+	elif(args.expense): print("The Expense flag has called", args.expense)
+	else:	print(cliargs.getParser().print_help())
